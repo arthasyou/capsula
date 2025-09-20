997e93a03c6ffe74dae0950df33c65185d4bb066
Ancient <arthas.you@163.com>
2025-09-20 10:52:59 +0800

    feat: deleting unuse code

--- crates/capsula-core/src/lib.rs
+++ crates/capsula-core/src/lib.rs
@@ -1,16 +1,11 @@
-// pub mod api;
-pub mod capsule;
-
 pub mod audit;
 pub mod block;
+pub mod capsule;
 pub mod error;
 pub mod integrity;
 pub mod keyring;
 pub mod policy;
 pub mod types;
-
-// pub mod decapsulator;
-// pub mod encapsulator;
 
 // Re-export commonly used types
 // pub use api::{create_medical_capsule, decrypt_medical_capsule_rsa, CapsulaApi};
